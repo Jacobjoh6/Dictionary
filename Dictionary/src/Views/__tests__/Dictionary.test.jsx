8e6036275c7d79b656c953ca5ecaa87905f2c0b4
Jacobjoh6 <114565026+Jacobjoh6@users.noreply.github.com>
2023-09-27 14:17:34 +0200

    fdsfds

--- Dictionary/src/Views/__tests__/Dictionary.test.jsx
+++ Dictionary/src/Views/__tests__/Dictionary.test.jsx
@@ -15,3 +15,24 @@ describe('Dictionary', () => {
   });
 });
 
+test('search for a word', async () => {
+  // Find input and button elements
+  const input = getByPlaceholderText('Search word');
+  const searchButton = getByText('Search');
+
+  // Type a word in the input and click the search button
+  await type(input, 'testword');
+  await click(searchButton);
+
+  // Wait for data to be loaded (assuming your component uses async API calls)
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Adjust the delay as needed
+
+  // Check if the word is displayed in the result
+  const wordResult = getByText('Word: testword');
+  expect(wordResult).not.toBeNull();
+
+  // Check if the error message is not displayed
+  const errorMessage = queryByText(/Error:/);
+  expect(errorMessage).toBeNull();
+});
+
